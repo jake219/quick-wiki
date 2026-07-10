207cee4948c5413519cb6c1b70ad426333cc161c
jake219 <75194433+jake219@users.noreply.github.com>
2026-07-10 12:45:11 -0500

    Delete ItemInfoConfig.java

--- src/main/java/com/example/ItemInfoConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-
-@ConfigGroup("iteminfo")
-public interface ItemInfoConfig extends Config
-{
-}
